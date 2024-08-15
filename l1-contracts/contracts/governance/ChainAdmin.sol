abad9911d89aaf42665c94952ab6219c9dba3121
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-08-15 11:37:13 +0200

    polish things up

--- l1-contracts/contracts/governance/ChainAdmin.sol
+++ l1-contracts/contracts/governance/ChainAdmin.sol
@@ -2,52 +2,60 @@
 
 pragma solidity 0.8.24;
 
-import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {IChainAdmin} from "./IChainAdmin.sol";
 import {IRestriction} from "./IRestriction.sol";
-import { Call } from "./Common.sol";
+import {Call} from "./Common.sol";
 
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 /// @author Matter Labs
 /// @custom:security-contact security@matterlabs.dev
 /// @notice The contract is designed to hold the `admin` role in ZKSync Chain (State Transition) contracts.
 /// The owner of the contract can perform any external calls and also save the information needed for
 /// the blockchain node to accept the protocol upgrade.
-contract ChainAdmin is IChainAdmin, Ownable2Step {
+contract ChainAdmin is IChainAdmin {
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    /// @notice Ensures that only the `ChainAdmin` contract itself can call the function.
+    /// @dev All functions that require access-control should use `onlySelf` modifier, while the access control logic
+    /// should be implemented in the restriction contracts.
     modifier onlySelf {
         require(msg.sender == address(this), "Only self");
         _;
     }
 
-    constructor(address _initialOwner) {
-        // solhint-disable-next-line gas-custom-errors, reason-string
-        require(_initialOwner != address(0), "Initial owner should be non zero address");
-        _transferOwnership(_initialOwner);
+    constructor(address[] memory _initialRestrictions) {
+        unchecked {
+            for(uint256 i = 0; i < _initialRestrictions.length; ++i) {
+                _addRestriction(_initialRestrictions[i]);
+            }   
+        }
     }
 
     /// @notice Mapping of protocol versions to their expected upgrade timestamps.
     /// @dev Needed for the offchain node administration to know when to start building batches with the new protocol version.
     mapping(uint256 protocolVersion => uint256 upgradeTimestamp) public protocolVersionToUpgradeTimestamp;
 
+    /// @notice The set of active restrictions.
     EnumerableSet.AddressSet internal activeRestrictions;    
 
+    /// @notice Returns the list of active restrictions.
     function getRestrictions() public view returns (address[] memory) {
         return activeRestrictions.values();
     }
 
+    /// @notice Checks if the restriction is active.
     function isRestrictionActive(address restriction) external view returns (bool) {
         return activeRestrictions.contains(restriction);
     }
 
-    function addRestriction(address restriction) external onlyOwner {
-        activeRestrictions.add(restriction);
+    /// @notice Adds a new restriction to the active restrictions set.
+    function addRestriction(address restriction) external onlySelf {
+        _addRestriction(restriction);
     }
 
-    // Note that it is `onlySelf` because some restrictions may not allow to remove themselves
+    /// @notice Removes a restriction from the active restrictions set.
+    /// @dev Sometimes restrictions might need to enforce their permanence (e.g. if a chain should be a rollup forever).
     function removeRestriction(address restriction) external onlySelf {
         activeRestrictions.remove(restriction);
     }
@@ -55,7 +63,7 @@ contract ChainAdmin is IChainAdmin, Ownable2Step {
     /// @notice Set the expected upgrade timestamp for a specific protocol version.
     /// @param _protocolVersion The ZKsync chain protocol version.
     /// @param _upgradeTimestamp The timestamp at which the chain node should expect the upgrade to happen.
-    function setUpgradeTimestamp(uint256 _protocolVersion, uint256 _upgradeTimestamp) external onlyOwner {
+    function setUpgradeTimestamp(uint256 _protocolVersion, uint256 _upgradeTimestamp) external onlySelf {
         protocolVersionToUpgradeTimestamp[_protocolVersion] = _upgradeTimestamp;
         emit UpdateUpgradeTimestamp(_protocolVersion, _upgradeTimestamp);
     }
@@ -64,7 +72,8 @@ contract ChainAdmin is IChainAdmin, Ownable2Step {
     /// @param _calls Array of Call structures defining target, value, and data for each call.
     /// @param _requireSuccess If true, reverts transaction on any call failure.
     /// @dev Intended for batch processing of contract interactions, managing gas efficiency and atomicity of operations.
-    function multicall(Call[] calldata _calls, bool _requireSuccess) external payable onlyOwner {
+    /// @dev Note, that this function lacks access control. It is expected that the access control is implemented in a separate restriction contract.
+    function multicall(Call[] calldata _calls, bool _requireSuccess) external payable {
         // solhint-disable-next-line gas-custom-errors
         require(_calls.length > 0, "No calls provided");
         // solhint-disable-next-line gas-length-in-loops
@@ -92,9 +101,15 @@ contract ChainAdmin is IChainAdmin, Ownable2Step {
         address[] memory restrictions = getRestrictions();
 
         unchecked {
-            for (uint256 i = 0; i < restrictions.length; i++) {
-                IRestriction(restrictions[i]).validateCall(_call);
+            for (uint256 i = 0; i < restrictions.length; ++i) {
+                IRestriction(restrictions[i]).validateCall(_call, msg.sender);
             }
         } 
+    }
+
+    /// @notice Adds a new restriction to the active restrictions set.
+    /// @param _restriction The address of the restriction contract to be added.
+    function _addRestriction(address _restriction) internal {
+        activeRestrictions.add(_restriction);
     }
 }
